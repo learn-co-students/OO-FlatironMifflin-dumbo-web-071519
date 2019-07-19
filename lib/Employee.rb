9076c4f23fd69e558424953cc587d7e7c3aa4d17
Flatiron School <oscar_ochoac@yahoo.com>
2019-07-19 17:54:55 -0400

    Changes / Completion

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,41 @@
 class Employee
 
+    attr_reader :name, :salary, :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+
+        @@all << self
+    end
+
+    def manager_name
+        manager.name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(fixnum)
+        self.all.select {|employee| employee.salary > fixnum }
+    end
+
+    def self.find_by_department(string)
+        Employee.all.find do |employee|
+            if employee.manager.department == string
+                return employee
+            end
+        end
+    end
+
+    def tax_bracket
+        Employee.all.select do |employee|
+            self.salary.between?(employee.salary - 1000,employee.salary + 1000)
+        end
+    end
+
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,39 @@
 class Manager
 
+    attr_reader :name, :department, :age
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+
+        @@all << self
+
+    end
+
+    def employees
+        Employee.all.select {|employee| employee.manager == self }
+    end
+
+    def self.all
+        @@all
+    end
+
+    def hire_employee(string, fixnum)
+        Employee.new(string, fixnum, self)
+    end
+
+    def self.all_departments
+        Manager.all.map{|manager| manager.department}
+    end
+
+    def self.average_age #Check if there is more efficient way
+        total_age = 0.0
+        ages = Manager.all.map {|manager| total_age += manager.age}
+        total_age /= ages.size
+        return total_age
+    end
+
 end
