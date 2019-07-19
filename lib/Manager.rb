4fad49266654e57db321823052c0a75ad53dd800
Ashna Shah-Grover <ashnashahgrover777@gmail.com>
2019-07-19 17:42:14 -0400

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,35 @@
 class Manager
 
+  attr_reader :name, :department, :age
+
+  @@all = []
+
+  def initialize(name, department, age)
+    @name = name
+    @department = department
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def employees
+    Employee.all.select {|employee| employee.manager_name == self.name}
+  end
+
+  def hire_employee(name, salary)
+    Employee.new(name, salary, self)
+  end
+
+  def self.all_departments
+    self.all.map {|manager| manager.departmentd}
+  end
+
+  def self.average_age
+    ages = self.all.map {|manager| manager.age}
+    ages.sum.to_f / ages.size
+  end
+
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,17 @@ require 'pry'
 
 #Test your code here
 
+goldie = Manager.new("Goldie", "Diffusion", 27)
+lila = Employee.new("Lila", 25000, goldie)
+naomi = Employee.new("Naomi", 75000, goldie)
+
+maxwell = Manager.new("Maxwell", "Architecture", 42)
+doddard = Employee.new("Doddard", 120000, maxwell)
+mista = Employee.new("Mista", 20000, maxwell)
+
+Hope = Manager.new("Hope", "Bright Apple", 61)
+
+
 
 binding.pry
 puts "done"
