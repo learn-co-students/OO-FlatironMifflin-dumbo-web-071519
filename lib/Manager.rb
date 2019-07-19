c3e062058ff10d4bd9b239ee8a20c6b928dd33e5
Gulnoza Muminova <gmcharmy@gmail.com>
2019-07-19 16:02:31 -0400

    Done

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,41 @@
 class Manager
+    @@all=[]
+    @@all_departments=[]
+    @@all_age=[]
+    attr_reader :name, :age, :department
+    
+    def initialize(name,age,department)
+       @name=name
+       @department=department
+       @age=age
+       @@all << self
+       @@all_departments << department
+       @@all_age << age
+    end
 
+    def employees
+       Employee.all.select do |employee|
+         employee.manager == self
+       end
+    end
+
+    def self.all
+       @@all
+    end
+
+    def hire_employee(name, salary)
+       Employee.new(name,salary,self)
+    end
+
+    def self.all_departments
+       @@all_departments
+    end
+   
+    def self.average_age
+      sum=0
+      @all_age.map do |age|
+        sum+=age
+      end
+      sum/@@all_age.count
+    end
 end
