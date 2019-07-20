a54b5493d69ad8d773fbfe73fefc55b73dfad346
vertreesa <Alessandra.Vertrees82@myhunter.cuny.edu>
2019-07-20 11:07:29 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,32 @@
 class Employee
+# **`Employee`**
 
+#   * `Employee.paid_over`
+#     * takes a `Fixnum` argument and returns an `Array` of all the employees whose salaries are over that amount
+#   * `Employee.find_by_department`
+#     * takes a `String` argument that is the name of a department and returns the first employee whose manager is working in that department
+#   * `Employee#tax_bracket`
+#     * returns an `Array` of all the employees whose salaries are within $1000 (± 1000) of the employee who invoked the method
+    attr_accessor :name, :salary, :manager
+    @@all = []
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def manager_name
+        @manager.name
+    end
+
+    def paid_over 
+        @@all.select do |employee|
+            employee.salary > @salary
+        end
+    end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,16 @@ require 'pry'
 
 
 #Test your code here
+michael = Manager.new("Michael", "paper", 50)
+lisa = Manager.new("Lisa", "technology", 32)
+bill = Manager.new("Bill", "human resources", 46)
 
+tom = Employee.new("Tom", 20000, michael)
+pam = Employee.new("Pam", 23000, bill)
+george = Employee.new("George", 5000, lisa)
+alice = Employee.new("Alice", 50000, michael)
+kevin = Employee.new("Kevin", 70000, lisa)
+paul = Employee.new("Paul", 12000, bill)
 
 binding.pry
 puts "done"
