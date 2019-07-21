be3c95d61baceb5c87e16b44656b5f3640c206ff
janetmndz <janetmendez.03@gmail.com>
2019-07-20 21:43:52 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,38 @@
 class Employee
+    @@all = []
+    
+    attr_reader :name, :salary, :manager
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+
+        @@all << self
+    end
+
+    def manager_name
+        self.manager.name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(salary)
+       self.all.select{|employee| employee.salary > salary} 
+    end
+
+    def self.find_by_department(department)
+        self.all.select{|employee| employee.manager.department == department}
+    end
+
+    def tax_bracket
+        min = self.salary-1000
+        max = self.salary+1000
+        Employee.all.select do |employee|
+            employee.salary.between?(min,max) && employee != self
+        end
+    end
 
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,19 @@ require 'pry'
 
 
 #Test your code here
+  
+pam = Manager.new("Pam", "Finanial Dep", 35)
+john = Manager.new("Kohn", "HR Dep", 38)
+doogs = Manager.new("Doogs", "Paper Dep", 31)
 
+
+willow = Employee.new("Willow", 15000, pam)
+joe = Employee.new("Joe", 18000, john)
+sam = Employee.new("Sam", 21000, doogs)
+scott = Employee.new("Scott", 23000, pam)
+clarke = Employee.new("Clarke", 23000, doogs)
+lisa = Employee.new("Lisa", 22000, john)
+sarah = Employee.new("Sarah", 25000, john)
 
 binding.pry
 puts "done"
