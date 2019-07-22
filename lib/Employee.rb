0f6073406598d01f428fd171997a8a92a9f69704
Ross Perkel <rossperkel@gmail.com>
2019-07-21 22:44:34 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,33 @@
 class Employee
+    @@all = []
+    attr_reader  :name
+    attr_accessor :manager, :salary
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def manager_name
+        @manager
+    end
+
+    def self.paid_over(num)
+        @@all.select {|employee| employee.salary > num}
+    end
+
+    def self.find_by_department(department)
+        @@all.find {|employee| employee.manager.department == department}
+    end
+
+    def tax_bracket
+        Employee.all.select{|employee| (self.salary-1000..self.salary+1000) === employee.salary}
+        end
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,34 @@
+require 'pry'
 class Manager
+    @@all = []
+    attr_reader :name, :age, :department
+
+
+    def initialize(name, age, department)
+        @name=name
+        @age = age
+        @department = department
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def employees
+        Employee.all.map {|employee| employee.manager == self.name}
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, age, self)
+    end
+
+    def self.all_departments
+        @@all.map(&:department)
+    end
+
+    def self.average_age
+        @@all.map{|manager| manager.age}.reduce{|sum, el| sum += el}.to_f / @@all.count 
+    end
 
 end
